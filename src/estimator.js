6e35f6ed90dbad5d43c17e9171a8fb0dc45c91b9
JOEL KIGGWE <joelkiggwe94@gmail.com>
2020-04-08 21:01:30 +0300

    Challenge 2 and 3 done.

--- src/estimator.js
+++ src/estimator.js
@@ -3,19 +3,42 @@ const covid19ImpactEstimator = (data) => {
   const impact = {};
   const severeImpact = {};
   
-  const { reportedCases, timeToElapse } = data;
-  
-  impact.currentlyInfected = reportedCases * 10;
-  severeImpact.currentlyInfected = reportedCases * 50;
- 
+  const { reportedCases, timeToElapse, totalHospitalBeds,
+     avgDailyIncomePopulation } = data;
+    
   // For impact
+  impact.currentlyInfected = reportedCases * 10;
   impact.infectionsByRequestedTime = computeInfectedPersonsThen(timeToElapse, impact.currentlyInfected, periodType);
-  
+  impact.severeCasesByRequestedTime = computeSevereCases(impact.infectionsByRequestedTime);
+
+  impact.hospitalBedsByRequestedTime = availableBedsThen(impact.severeCasesByRequestedTime ,totalHospitalBeds);
+
+   impact.casesForICUByRequestedTime = 
+       severePositiveCases(impact.infectionsByRequestedTime);
+
+    impact.casesForVentilationByRequestedTime = 
+    severePositiveCasesForVentilation( impact.infectionsByRequestedTime );
+    
+    impact.dollarsInFlight = computeEconomicLoss(avgDailyIncomePopulation,impact.infectionsByRequestedTime,avgIncomeInUSD, timeToElapse, periodType );
+    
   // For severeImpact
+  severeImpact.currentlyInfected = reportedCases * 50;
   severeImpact.infectionsByRequestedTime = computeInfectedPersonsThen(timeToElapse, severeImpact.currentlyInfected, periodType);
   
+  severeImpact.severeCasesByRequestedTime = computeSevereCases(severeImpact.infectionsByRequestedTime );
 
-      // Function computes number of infected persons
+  severeImpact.hospitalBedsByRequestedTime = availableBedsThen(severeImpact.severeCasesByRequestedTime ,totalHospitalBeds);
+
+  severeImpact.casesForICUByRequestedTime = 
+  severePositiveCases(severeImpact.infectionsByRequestedTime);
+
+ severeImpact.casesForVentilationByRequestedTime = 
+  severePositiveCasesForVentilation( severeImpact.infectionsByRequestedTime );
+
+  severeImpact.dollarsInFlight = computeEconomicLoss(avgDailyIncomePopulation,severeImpact.infectionsByRequestedTime,avgIncomeInUSD, timeToElapse, periodType );
+
+    
+  // Function computes number of infected persons
   function computeInfectedPersonsThen( timeFactor, currentlyInfected, periodType ){
     let _factor = 0;
     switch (periodType){
@@ -38,6 +61,62 @@ const covid19ImpactEstimator = (data) => {
     return parseInt(factor/3);
   }
 
+      // Function computes severeCases
+  function computeSevereCases( infected ){
+     return 0.15 * infected;
+  }
+
+  // Function computes number of avaialble beds for the severe cases
+  function availableBedsThen( severeCases, totalHospitalBeds ){
+     // Basing on 35% availability
+     let hospitalBedsAvailableThen = 0;
+      let hospitalBedsAvailable = 0;
+
+     hospitalBedsAvailable =  0.35 * totalHospitalBeds;
+
+     // After severe case consideration
+      hospitalBedsAvailableThen = hospitalBedsAvailable - severeCases;
+       return hospitalBedsAvailableThen;
+  }
+
+    // Function that  computes severe Positive Cases in need for ICU 
+    function severePositiveCasesForICU( severe ){
+       return 0.05* severe;
+    }
+
+    // Function that computes severe positive cases in need for ventilation
+    function severePositiveCasesForVentilation( severe ){
+       return 0.02 * severe;
+    }
+
+    // Function that computes the money lost in a certain period
+    function computeEconomicLoss( majority, infected, avgIncome, timeFactor, periodType){
+        let dollarsInFlight = 0;
+        let avgLossFactor = 0;
+        let numberOfDays = 0;
+
+        switch (periodType){
+          case "days":
+            _factor = computeEconomicLossFactor(timeFactor,avgIncome);
+          case "weeks":
+          numberOfDays = 7*timeFactor;
+          avgLossFactor =  computeEconomicLossFactor(numberOfDays, avgIncome);
+          case "months":
+            numberOfDays = 30*timeFactor;
+            avgLossFactor = computeEconomicLossFactor(numberOfDays, avgIncome);
+        } 
+
+   dollarsInFlight = infected * majority * avgLossFactor;
+        
+      return dollarsInFlight;
+    }
+
+    // Function computes total amount in a certain period of time(days)
+    // from a time frame in days, weeks, months
+    function computeEconomicLossFactor(timeFactor, avgIncome){
+       return avgIncome * timeFactor;
+        
+    }
   return {
     data: input,
     impact: {},
