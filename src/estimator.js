ceb9ce361daf91d62d9b4a4b182e53f8d7879056
KenjxJJ <49744118+KenjxJJ@users.noreply.github.com>
2020-04-13 22:38:33 +0300

    Initial Commit (#1)
    * Initial Commit
    
    * Challenge 1 done
    
    * Challenge 2 and 3 done.
    
    * Refactor  for challenges 2 and 3
    
    * Delete run.js
    
    * Edit the return objects
    
    * Edit the return objects
    
    * Revert "Merge branch 'assesement' of https://github.com/KenjxJJ/covid-19-estimator-js into assesement"
    
    This reverts commit 5b21cbff7e40d2b8f99164cfb83306a647868f8c, reversing
    changes made to d0172e6bdc03a2a7909f608f31adb4e0160f3373.
    
    	new file:   src/run.js
    
    * Revert "Edit the return objects"
    
    This reverts commit 01387b6a2074a5442a7eeb761b0682fce2c87a0b.
    
    * Revert "Revert "Edit the return objects""
    
    This reverts commit 45048c971f8adafde679d05c5536f0901efabfda.
    
    * Change property of impact and severeImpact
    
    * Remove run.js after revert
    
    * Refactor code to include parseInt function
    
    * Change to rounding off function in estimator file
    
    * Update on the availableHospitalThen function
    
    * Update the availableHospitalThen function#2
    
    * Edit the property to casesForVentilators
    
    * Add parentheses two functions dollarsInFlight
    
    * Update computeEconomicLoss with a default
    
    * Update computeEconomicLoss #2
    
    * Remove git function
    
    * Updated  gradr.yml
    
    * Update using trunc method#Challenge2
    
    * Update Challenge2 and Challenge3 output
    
    * Change function for challenge 3
    
    * Upadate with the rounding function
    
    * Update code with truncation #Challenge_3
    
    * Add UI to the estimator
    
    * Remove index.html and other unwanted files
    
    * Add url to the UI

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,135 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  const impact = {};
+  const severeImpact = {};
+
+  const {
+    reportedCases, timeToElapse, totalHospitalBeds,
+    region, periodType
+  } = data;
+
+  // Function computes infectionRate
+  function computeInfectionFactor(factor) {
+    return parseInt(factor / 3, 10);
+  }
+
+  // Function computes number of infected persons
+  function computeInfectedPersonsThen(timeFactor, currentlyInfected, periodTypeDays) {
+    let factor = 0;
+    let numberOfDays = 0;
+    switch (periodTypeDays) {
+      case 'days':
+        factor = computeInfectionFactor(timeFactor);
+        break;
+      case 'weeks':
+        numberOfDays = 7 * timeFactor;
+        factor = computeInfectionFactor(numberOfDays);
+        break;
+      case 'months':
+        numberOfDays = 30 * timeFactor;
+        factor = computeInfectionFactor(numberOfDays);
+        break;
+      default:
+    }
+
+    const infectionsByRequestedTime = currentlyInfected * (2 ** factor);
+    return infectionsByRequestedTime;
+  }
+
+  // Function computes severeCases
+  function computeSevereCases(infected) {
+    return Math.trunc(0.15 * infected);
+  }
+
+  // Function computes number of avaialble beds for the severe cases
+  function availableBedsThen(severeCases, totalHospitalBedsAvailable) {
+    // Basing on 35% availability
+    let hospitalBedsAvailableThen = 0;
+    let hospitalBedsAvailable = 0;
+
+    hospitalBedsAvailable = Math.ceil(0.35 * totalHospitalBedsAvailable);
+
+    // After severe case consideration
+    hospitalBedsAvailableThen = hospitalBedsAvailable - severeCases;
+    return hospitalBedsAvailableThen;
+  }
+
+  // Function that  computes severe Positive Cases in need for ICU
+  function severePositiveCasesForICU(severe) {
+    return Math.trunc(0.05 * severe);
+  }
+
+  // Function that computes severe positive cases in need for ventilation
+  function severePositiveCasesForVentilation(severe) {
+    return Math.trunc(0.02 * severe);
+  }
+
+  // Function that computes the money lost in a certain period
+  function computeEconomicLoss(majority, infected, avgIncome, timeFactor, periodTypeDays) {
+    let dollarsInFlight = 0;
+    let numberOfDays = 0;
+    switch (periodTypeDays) {
+      case 'days':
+        dollarsInFlight = Math.trunc((majority * infected * avgIncome) / timeFactor);
+        break;
+      case 'weeks':
+        numberOfDays = 7 * timeFactor;
+        dollarsInFlight = Math.trunc((majority * infected * avgIncome) / numberOfDays);
+        break;
+      case 'months':
+        numberOfDays = 30 * timeFactor;
+        dollarsInFlight = Math.trunc((majority * infected * avgIncome) / numberOfDays);
+        break;
+      default:
+    }
+    return dollarsInFlight;
+  }
+
+  // For impact
+  impact.currentlyInfected = reportedCases * 10;
+  impact.infectionsByRequestedTime = computeInfectedPersonsThen(timeToElapse,
+    impact.currentlyInfected, periodType);
+  impact.severeCasesByRequestedTime = computeSevereCases(impact.infectionsByRequestedTime);
+
+  impact.hospitalBedsByRequestedTime = availableBedsThen(impact.severeCasesByRequestedTime,
+    totalHospitalBeds);
+
+  impact.casesForICUByRequestedTime = severePositiveCasesForICU(impact.infectionsByRequestedTime);
+
+  impact.casesForVentilatorsByRequestedTime = severePositiveCasesForVentilation(
+    impact.infectionsByRequestedTime
+  );
+
+  impact.dollarsInFlight = computeEconomicLoss(region.avgDailyIncomePopulation,
+    impact.infectionsByRequestedTime, region.avgDailyIncomeInUSD, timeToElapse, periodType);
+
+  // For severeImpact
+  severeImpact.currentlyInfected = reportedCases * 50;
+  severeImpact.infectionsByRequestedTime = computeInfectedPersonsThen(timeToElapse,
+    severeImpact.currentlyInfected, periodType);
+
+  severeImpact.severeCasesByRequestedTime = computeSevereCases(
+    severeImpact.infectionsByRequestedTime
+  );
+  severeImpact.hospitalBedsByRequestedTime = availableBedsThen(
+    severeImpact.severeCasesByRequestedTime, totalHospitalBeds
+  );
+
+  severeImpact.casesForICUByRequestedTime = severePositiveCasesForICU(
+    severeImpact.infectionsByRequestedTime
+  );
+
+  severeImpact.casesForVentilatorsByRequestedTime = severePositiveCasesForVentilation(
+    severeImpact.infectionsByRequestedTime
+  );
+
+  severeImpact.dollarsInFlight = computeEconomicLoss(region.avgDailyIncomePopulation,
+    severeImpact.infectionsByRequestedTime, region.avgDailyIncomeInUSD, timeToElapse, periodType);
+  return {
+    data: input,
+    impact,
+    severeImpact
+  };
+};
 
 export default covid19ImpactEstimator;
